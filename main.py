aabc8c8e870cbc5396825417c869cf13553b990d
Its-CNX <calyx.versas@gmail.com>
2023-02-26 23:03:19 +0100

    Created /purge
    Created a /purge command that purges a given amount of messages to delete and responds with a Rich presence style Embed.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import datetime
 import dotenv
 import os
 
@@ -12,7 +13,10 @@ async def on_ready():
 
 @bot.slash_command(name = "purge", description = "Purges a set amount of messages.")
 async def purge(ctx, limit : int):
+    embed = discord.Embed(color=discord.Colour.blurple(), title=f"{limit} Messages were purged.", timestamp=datetime.datetime.now())
+    embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1048935994323370024/1079520156398653450/v-letter-logo-icon-for-business-and-company-vector.jpg")
+    embed.set_author(name=bot.user.name, icon_url="https://cdn.discordapp.com/attachments/1048935994323370024/1079520156398653450/v-letter-logo-icon-for-business-and-company-vector.jpg")
     await ctx.channel.purge(limit = limit)
-    await ctx.send(f"{limit} messages have been deleted by {ctx.user}.")
+    await ctx.respond(embed=embed)
 
 bot.run(os.getenv('TOKEN'))
